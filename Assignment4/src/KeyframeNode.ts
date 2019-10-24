6bcd53f2dd60a522267eb89160ed3df3d36a0737
Sahith Nallapareddy <sahith.reddy@gmail.com>
2019-10-24 18:18:02 -0400

    assignment 4

---
+++ Assignment4/src/KeyframeNode.ts
@@ -0,0 +1,191 @@
+import { SGNode } from "./SGNode";
+import { mat4, vec3 } from "gl-matrix";
+import { Scenegraph } from "./Scenegraph";
+import { Stack } from "%COMMON/Stack";
+import { ScenegraphRenderer } from "./ScenegraphRenderer";
+import { IVertexData } from "%COMMON/IVertexData";
+
+/**
+ * This node represents a transformation in the scene graph. It has only one child. The
+ * transformation can be viewed as changing from its child's coordinate system to its parent's
+ * coordinate system. This also stores an animation transform that can be tweaked at runtime
+ * @author Amit Shesh
+ */
+export class KeyframeNode extends SGNode {
+  /**
+   * Matrices storing the static and animation transformations separately, so that they can be
+   * changed separately
+   */
+  protected transform: mat4;
+  protected animationTransform: mat4;
+  private keyFrames: number[][];
+  private time: number;
+
+  /**
+   * A reference to its only child
+   */
+  protected child: SGNode;
+
+  public constructor(graph: Scenegraph<IVertexData>, name: string) {
+    super(graph, name);
+    this.transform = mat4.create();
+    this.animationTransform = mat4.create();
+    this.keyFrames = [];
+    this.child = null;
+    this.time = 0;
+  }
+
+  /**
+   * Creates a deep copy of the subtree rooted at this node
+   * @return a deep copy of the subtree rooted at this node
+   */
+  public clone(): SGNode {
+    let newchild: SGNode;
+
+    if (this.child != null) {
+      newchild = this.child.clone();
+    } else {
+      newchild = null;
+    }
+
+    let newtransform: KeyframeNode = new KeyframeNode(
+      this.scenegraph,
+      this.name
+    );
+    newtransform.setTransform(this.transform);
+    newtransform.setAnimationTransform(this.animationTransform);
+
+    if (newchild != null) {
+      try {
+        newtransform.addChild(newchild);
+      } catch (e) {}
+    }
+    return newtransform;
+  }
+
+  /**
+   * Determines if this node has the specified name and returns itself if so. Otherwise it recurses
+   * into its only child
+   * @param name name of node to be searched
+   * @return
+   */
+  public getNode(name: string): SGNode {
+    let n: SGNode = super.getNode(name);
+    if (n != null) return n;
+
+    if (this.child != null) {
+      return this.child.getNode(name);
+    }
+
+    return null;
+  }
+
+  /**
+   * Since this node can have a child, it override this method and adds the child to itself
+   * This will overwrite any children set for this node previously.
+   * @param child the child of this node
+   * @throws IllegalArgumentException this method does not throw this exception
+   */
+  public addChild(child: SGNode): void {
+    if (this.child != null)
+      throw new Error("Transform node already has a child");
+    this.child = child;
+    this.child.setParent(this);
+  }
+
+  /**
+   * Draws the scene graph rooted at this node
+   * After preserving the current top of the modelview stack, this "post-multiplies" its
+   * animation transform and then its transform in that order to the top of the model view
+   * stack, and then recurses to its child. When the child is drawn, it restores the modelview
+   * matrix
+   * @param context the generic renderer context {@link sgraph.IScenegraphRenderer}
+   * @param modelView the stack of modelview matrices
+   */
+
+  public draw(context: ScenegraphRenderer, modelView: Stack<mat4>) {
+    modelView.push(mat4.clone(modelView.peek()));
+    mat4.multiply(modelView.peek(), modelView.peek(), this.animationTransform);
+    const v = this.keyFrames[this.time];
+    if (v) {
+      const v2 = this.keyFrames[(this.time + 1) % this.keyFrames.length];
+      const up = vec3.fromValues(v[3], v[4], v[5]);
+      const currentPosition = vec3.fromValues(v[0], v[1], v[2]);
+      const nextPosition = vec3.fromValues(v2[0], v2[1], v2[2]);
+      const look = mat4.targetTo(
+        mat4.create(),
+        vec3.fromValues(0, 0, 0),
+        vec3.sub(vec3.create(), nextPosition, currentPosition),
+        up
+      );
+      mat4.translate(modelView.peek(), modelView.peek(), currentPosition);
+      mat4.multiply(modelView.peek(), modelView.peek(), look);
+
+      this.time = this.time + 1;
+      this.time = this.time % this.keyFrames.length;
+    }
+    mat4.multiply(modelView.peek(), modelView.peek(), this.transform);
+
+    if (this.child != null) this.child.draw(context, modelView);
+    modelView.pop();
+  }
+
+  /**
+   * Sets the animation transform of this node
+   * @param mat the animation transform of this node
+   */
+  public setAnimationTransform(mat: mat4): void {
+    this.animationTransform = mat;
+  }
+
+  /**
+   * Gets the transform at this node (not the animation transform)
+   * @return
+   */
+  public getTransform(): mat4 {
+    return this.transform;
+  }
+
+  /**
+   * Sets the transformation of this node
+   * @param t
+   * @throws IllegalArgumentException
+   */
+  public setTransform(t: mat4): void {
+    this.transform = mat4.clone(t);
+  }
+
+  /**
+   * Gets the animation transform of this node
+   * @return
+   */
+  public getAnimationTransform(): mat4 {
+    return this.animationTransform;
+  }
+
+  /**
+   * Sets the scene graph object of which this node is a part, and then recurses to its child
+   * @param graph a reference to the scenegraph object of which this tree is a part
+   */
+  public setScenegraph(graph: Scenegraph<IVertexData>): void {
+    super.setScenegraph(graph);
+    if (this.child != null) {
+      this.child.setScenegraph(graph);
+    }
+  }
+
+  public setKeyFrames(keyFramesFile: string): void {
+    fetch(keyFramesFile)
+      .then(response => response.text())
+      .then(data => {
+        let lines: string[] = data.split(/\r?\n/);
+        lines.shift();
+        lines.forEach(line => {
+          line = line.trim();
+          let l = line.split(/\s+/);
+          let v: number[] = l.map(x => parseFloat(x));
+          this.keyFrames.push(v);
+        });
+      });
+  }
+}
---
+++ Assignment4/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/Assignment4.ts", //entry point of application
+  mode: "development",
+  watch: true, //enable to debug (step through)
+  module: {
+    //details about grabbing modules to export
+    rules: [
+      {
+        test: /\.tsx?$/, //use all files with this extension
+        use: "ts-loader", //pass to this program (Typescript transpiling + loading)
+        exclude: /node_modules/ //exclude the folder with installed packages
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"], //apply this rule to all files with these extensions
+    alias: {
+      //alias to manage relative paths
+      "%COMMON": path.resolve(__dirname, "../common") //resolve alias to this relative path
+    }
+  },
+  output: {
+    filename: "assignment4.js", //name of the final bundled JS file
+    path: path.resolve(__dirname, "out") //put bundled file in this folder
+  },
+  devtool: "source-map" //enable source-map for debugging (stepping through). generates DrawWithIndices.js.map
+};
