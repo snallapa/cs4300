a30e5260a8513528dec186a2fe44e7de66e36a8f
Sahith Nallapareddy <sahith.reddy@gmail.com>
2019-11-11 16:17:03 -0500

    assignment 6

---
+++ Assignment6/src/Controller.ts
@@ -0,0 +1,19 @@
+import { View } from "View";
+import * as OBJ from "webgl-obj-loader";
+import { mat4 } from "gl-matrix";
+import { Material } from "%COMMON/Material";
+
+export interface Features {}
+export class Controller implements Features {
+  private view: View;
+
+  constructor(view: View) {
+    this.view = view;
+    this.view.setFeatures(this);
+  }
+
+  public go(): void {
+    this.view.initScenegraph();
+    this.view.draw();
+  }
+}
